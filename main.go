0cc9a61b008b83e0befccd7413acd7f1ea56de3c
Andrew Sun <andrew@crossbowffs.com>
2020-12-21 23:26:49 -0800

    Add monitor thread

--- main.go
+++ main.go
@@ -6,16 +6,20 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"path/filepath"
 	"regexp"
+	"time"
 )
 
 var portFlag = flag.Int("port", 80, "port to listen on")
-var storageFlag = flag.String("storage", "/tmp/fish", "where to store uploaded files")
-var maxSizeFlag = flag.Int64("maxsize", 200<<20, "maximum file upload request size")
+var uploadDirFlag = flag.String("uploaddir", "/tmp/fish", "where to store uploaded files")
+var maxSizeFlag = flag.Int64("maxsize", 200<<20, "maximum file upload request size in bytes")
+var expiryFlag = flag.Duration("expiry", 15*time.Minute, "delete uploaded files after this many secs")
 
 // Returns a cryptographically secure version 4 UUID.
 func uuid4() (string, error) {
@@ -47,56 +51,67 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	r.Body = http.MaxBytesReader(w, r.Body, *maxSizeFlag)
 
 	if r.Method != "POST" {
-		http.Error(w, "only POST requests accepted", http.StatusBadRequest)
+		msg := "only POST requests accepted"
+		log.Println(msg)
+		http.Error(w, msg, http.StatusBadRequest)
 		return
 	}
 
 	uploadedFile, header, err := r.FormFile("file")
 	if err != nil {
-		if err == http.ErrMissingFile {
-			http.Error(
-				w,
-				"missing `file` param, or param was not a file",
-				http.StatusBadRequest,
-			)
-		} else {
-			http.Error(w, err.Error(), http.StatusBadRequest)
+		msg := "missing `file` param, or param was not a file"
+		if err != http.ErrMissingFile {
+			msg = fmt.Sprintf("failed to get file from request: %v", err)
 		}
+		log.Println(msg)
+		http.Error(w, msg, http.StatusBadRequest)
 		return
 	}
 
 	fileID, err := uuid4()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		msg := fmt.Sprintf("failed to generate uuid: %v", err)
+		log.Println(msg)
+		http.Error(w, msg, http.StatusInternalServerError)
 		return
 	}
 
 	fileName := safeFileName(header.Filename)
-	dirPath := filepath.Join(*storageFlag, fileID)
+	dirPath := filepath.Join(*uploadDirFlag, fileID)
 	filePath := filepath.Join(dirPath, fileName)
 	urlPath := fmt.Sprintf("/download/%s/%s", fileID, url.PathEscape(fileName))
 
+	log.Printf("writing %s\n", filePath)
+
 	err = os.Mkdir(dirPath, os.ModePerm)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		msg := fmt.Sprintf("failed to create dir %s: %v", dirPath, err)
+		log.Println(msg)
+		http.Error(w, msg, http.StatusInternalServerError)
 		return
 	}
 
 	fsFile, err := os.Create(filePath)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		msg := fmt.Sprintf("failed to create file %s: %v", filePath, err)
+		log.Println(msg)
+		http.Error(w, msg, http.StatusInternalServerError)
 		return
 	}
 
 	_, err = io.Copy(fsFile, uploadedFile)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		msg := fmt.Sprintf("failed to write file %s: %v", filePath, err)
+		log.Println(msg)
+		http.Error(w, msg, http.StatusInternalServerError)
 		return
 	}
 
 	_, err = io.WriteString(w, urlPath)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		msg := fmt.Sprintf("failed to write response URL %s: %v", urlPath, err)
+		log.Println(msg)
+		http.Error(w, msg, http.StatusInternalServerError)
 		return
 	}
 }
@@ -135,22 +150,50 @@ func asAttachment(h http.Handler) http.Handler {
 	})
 }
 
+// Purges the upload directory of files older than the configured
+// expiry time.
+func monitor() {
+	for {
+		files, err := ioutil.ReadDir(*uploadDirFlag)
+		if err != nil {
+			log.Fatalf("failed to enumerate %s: %v\n", *uploadDirFlag, err)
+		}
+
+		now := time.Now()
+		for _, f := range files {
+			ts := f.ModTime()
+			if now.Sub(ts) > *expiryFlag {
+				path := filepath.Join(*uploadDirFlag, f.Name())
+				log.Printf("purging %s (created at %v)\n", path, ts)
+				err = os.RemoveAll(path)
+				if err != nil {
+					log.Printf("failed to purge %s: %v\n", path, err)
+				}
+			}
+		}
+
+		time.Sleep(1 * time.Minute)
+	}
+}
+
 func main() {
 	flag.Parse()
 
-	err := os.MkdirAll(*storageFlag, os.ModePerm)
+	err := os.MkdirAll(*uploadDirFlag, os.ModePerm)
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to create upload dir %s: %v\n", *uploadDirFlag, err)
 	}
+
+	go monitor()
 
 	http.Handle("/", http.FileServer(http.Dir("static")))
 	http.HandleFunc("/upload", uploadHandler)
 	http.Handle("/download/", http.StripPrefix("/download/", asAttachment(
-		http.FileServer(blindFileSystem{http.Dir(*storageFlag)}),
+		http.FileServer(blindFileSystem{http.Dir(*uploadDirFlag)}),
 	)))
 
 	err = http.ListenAndServe(fmt.Sprintf(":%d", *portFlag), nil)
 	if err != nil {
-		panic(err)
+		log.Fatalf("http server returned error: %v\n", err)
 	}
 }
